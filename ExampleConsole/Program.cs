f1ad9bce02937396a3af0da3672dca0fd28a638e
asabla <1063362+asabla@users.noreply.github.com>
2024-08-14 08:43:55 +0200

    Initial support for chatting with Ollama
    Expects llama3.1:8b model is available in ollama. If you don't already
    have it, use this command to download it: `ollama pull llama3.1:8b`

---
+++ ExampleConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// Currently endpoint override is experimental and causes a warning
+#pragma warning disable SKEXP0010
+var builder = Kernel
+    .CreateBuilder()
+    .AddOpenAIChatCompletion(
+        modelId: "llama3.1:8b",
+        apiKey: "sk12345",  // Ollama doesn't use API-keys, this can be anything
+        endpoint: new Uri("http://localhost:11434"));
+
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine() ?? string.Empty;
+
+    if (string.IsNullOrWhiteSpace(userInput))
+    {
+        Console.WriteLine("Error > Please enter a valid input.");
+        continue;
+    }
+
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
